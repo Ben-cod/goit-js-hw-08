b126bdb319661f2841854ce929a3a8043374c1e5
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-06-10 20:16:44 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,33 +2,13 @@
 import Player from '@vimeo/player';
 import { throttle } from 'lodash.throttle';
 
-const iframeRef = document.querySelector('#vimeo-player')
-const player = new Player(iframeRef);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-// Функція для збереження поточного часу відтворення у локальне сховище
-const saveCurrentTime = throttle(async () => {
-  try {
-    const currentTime = await player.getCurrentTime();
-    localStorage.setItem('videoplayer-current-time', currentTime);
-  } catch (error) {
-    console.error('Помилка при збереженні поточного часу відтворення:', error);
-  }
-}, 1000); // Оновлення не частіше, ніж раз на секунду
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-// Функція для відновлення відтворення з збереженої позиції
-const restorePlayback = async () => {
-  try {
-    const currentTime = localStorage.getItem('videoplayer-current-time');
-    if (currentTime) {
-      await player.setCurrentTime(currentTime);
-    }
-  } catch (error) {
-    console.error('Помилка при відновленні відтворення:', error);
-  }
-};
-
-
-player.on('timeupdate', saveCurrentTime);
-
-
-window.addEventListener('load', restorePlayback);
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
