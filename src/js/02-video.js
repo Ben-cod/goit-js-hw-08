0887b87b1b39fa0bba48407df224410102b87283
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-06-10 18:42:16 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
 
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframeRef = document.querySelector('#vimeo-player')
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+// Функція для збереження поточного часу відтворення у локальне сховище
+const saveCurrentTime = throttle(async () => {
+  try {
+    const currentTime = await player.getCurrentTime();
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  } catch (error) {
+    console.error('Помилка при збереженні поточного часу відтворення:', error);
+  }
+}, 1000); // Оновлення не частіше, ніж раз на секунду
+
+// Функція для відновлення відтворення з збереженої позиції
+const restorePlayback = async () => {
+  try {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime) {
+      await player.setCurrentTime(currentTime);
+    }
+  } catch (error) {
+    console.error('Помилка при відновленні відтворення:', error);
+  }
+};
+
+
+player.on('timeupdate', saveCurrentTime);
+
+
+window.addEventListener('load', restorePlayback);
