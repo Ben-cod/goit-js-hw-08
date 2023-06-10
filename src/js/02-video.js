2db4c3559584bfc3f4bbca58e1ce69d94f056ca6
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-06-10 20:19:17 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,33 @@
-
 import Player from '@vimeo/player';
-import { throttle } from 'lodash.throttle';
+import throttle from 'lodash.throttle';
+
+// --------------- 1st variant --------------------
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(currentTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
 });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+player.setColor('#45a247').then(function (color) {
+    // the color that was set
+}).catch(function (error) {
+    // an error occurred setting the color
